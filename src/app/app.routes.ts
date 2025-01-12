6eff4794eb217d0989593fec5529d30010fdd39a
Kavish Seechurn <mytkavish@hotmail.com>
2025-01-12 13:21:31 +0400

    addition of landing and signup page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingComponent} from '../components/landing/landing.component';
+import {SignupComponent} from '../components/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./components/landing/landing.component').then(m => m.LandingComponent) },
+  { path: 'signup', loadComponent: () => import('./components/signup/signup.component').then(m => m.SignupComponent) },
+  { path: '**', redirectTo: '' },
+];
