b125ac643da239feead1256db90f41ef499d9801
Kavish Seechurn <mytkavish@hotmail.com>
2025-03-04 07:13:33 +0400

    updated blood inventory

--- src/components/admin/blood-inventory/blood-inventory.component.ts
+++ src/components/admin/blood-inventory/blood-inventory.component.ts
@@ -2,14 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import { DashboardService } from '../../../services/dashboard.service';
 import { faUser, faHeart, faSyringe, faTasks, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
-import {DatePipe, NgForOf, NgStyle} from '@angular/common';
+import {DatePipe, NgClass, NgForOf, NgStyle} from '@angular/common';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-blood-inventory',
   imports: [
     DatePipe,
-    NgForOf
+    NgForOf,
+    NgClass
   ],
   templateUrl: './blood-inventory.component.html',
   styleUrl: './blood-inventory.component.scss'
@@ -40,5 +41,12 @@ export class BloodInventoryComponent implements OnInit {
       },
       error: (err) => console.error('Error loading expiry alerts:', err),
     });
+  }
+
+  isCritical(expiryDate: string): boolean {
+    const today = new Date();
+    const expiry = new Date(expiryDate);
+    const diff = (expiry.getTime() - today.getTime()) / (1000 * 60 * 60 * 24);
+    return diff <= 7; // Mark as critical if the expiry is within the next 7 days
   }
 }
